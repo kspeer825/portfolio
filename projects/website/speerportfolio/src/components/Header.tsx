9379d13fc906e6651168df9b6344f294c8d73689
Kyle Speer <kyle.speer@ophelia.com>
2023-01-24 21:26:07 -0500

    swap header link order

--- projects/website/speerportfolio/src/components/Header.tsx
+++ projects/website/speerportfolio/src/components/Header.tsx
@@ -12,14 +12,14 @@ function Header() {
         <h2> Kyle Speer </h2>
         <p> Hi! I'm a software engineer with interests in DevOps, testing, and woodworking. </p>
         <div className="App-header-links">
+          <a href={`mailto:kyle.d.speer@gmail.com?subject=""&body=""`}>
+            <img src={gmail_logo} className="App-logo" alt="GMail Logo" />
+          </a>
           <a href="https://www.linkedin.com/in/kyle-d-speer">
             <img src={linkedin_logo} className="App-logo" alt="LinkedIn Logo" />
           </a>
           <a href="https://github.com/kspeer825">
             <img src={github_logo} className="App-logo" alt="Github Logo" />
-          </a>
-          <a href={`mailto:kyle.d.speer@gmail.com?subject=""&body=""`}>
-            <img src={gmail_logo} className="App-logo" alt="GMail Logo" />
           </a>
         </div>
       </header>
